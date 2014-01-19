9fc9a3f1503b63d94fc001674d8377401eda35a7
Doug Blank <dblank@cs.brynmawr.edu>
2014-01-19 08:57:23 -0500

    Refined debugging output and headers

--- simple_kernel.py
+++ simple_kernel.py
@@ -1,6 +1,21 @@
 # simple_kernel.py
 # by Doug Blank <doug.blank@gmail.com>
 #
+# This sample kernel is meant to be able to demonstrate using zmq for
+# implementing a language backend (called a kernel) for IPython. It is
+# written in the most straightforward manner so that it can be easily
+# translated into other programming languages. It doesn't use any code
+# from IPython, but only standard Python libraries and zmq.
+#
+# It is also designed to be able to run, showing the details of the
+# message handling system.
+#
+# To adjust debug output, set debug_level to:
+#  0 - show no debugging information
+#  1 - shows basic running information
+#  2 - also shows loop details
+#  3 - also shows message details
+#
 # Start with a command, such as:
 # ipython console --KernelManager.kernel_cmd="['python', 'simple_kernel.py', 
 #                                              '{connection_file}']"
@@ -12,18 +27,26 @@ import sys
 import zmq
 import json
 import hmac
+import uuid
 import threading
+from pprint import pformat
 from zmq.eventloop import ioloop, zmqstream
 from zmq.error import ZMQError
 
-decoder = json.JSONDecoder()
-encoder = json.JSONEncoder()
+decode = json.JSONDecoder().decode
+encode = json.JSONEncoder().encode
+debug_level = 3 # 0 (none) to 3 (all) for various levels of detail
+
+def dprint(level, *args, **kwargs):
+    """ Show debug information """
+    if level <= debug_level:
+        print(*args, **kwargs)
 
 ## Initialize:
-print("Loading simple_kernel with args:", sys.argv)
-print("Reading config file '%s'..." % sys.argv[1])
-config = decoder.decode("".join(open(sys.argv[1]).readlines()))
-print("Config:", config)
+dprint(1, "Loading simple_kernel with args:", sys.argv)
+dprint(1, "Reading config file '%s'..." % sys.argv[1])
+config = decode("".join(open(sys.argv[1]).readlines()))
+dprint(1, "Config:", pformat(config))
 
 connection     = "tcp://" + config["ip"] + ":"
 heartbeat_conn = connection + str(config["hb_port"])
@@ -32,22 +55,30 @@ shell_conn     = connection + str(config["shell_port"])
 control_conn   = connection + str(config["control_port"])
 stdin_conn     = connection + str(config["stdin_port"])
 
-session_id = unicode(config["key"]).encode("ascii")
-auth = hmac.HMAC(session_id)
+session_id = unicode(uuid.uuid4()).encode('ascii')
+secure_key = unicode(config["key"]).encode("ascii")
+auth = hmac.HMAC(secure_key)
+execution_count = 1
 
 def iopub_handler(msg):
-    print("iopub received:", msg)
+    dprint(1, "iopub received:", msg)
+
+def msg_id():
+    """ Return a new uuid for message id """
+    return str(uuid.uuid4())
 
 def sign(msg_lst):
+    """
+    Sign a message with a secure signature.
+    """
     h = auth.copy()
     for m in msg_lst:
         h.update(m)
     return h.hexdigest()
 
-execution_count = 1
 def shell_handler(msg):
     global execution_count
-    print("shell received:", msg)
+    dprint(1, "shell received:", msg)
     # Shell message to handle:
     # ['90711B188AEF41E19FE80A2A788E75A4', 
     #  '<IDS|MSG>', 
@@ -60,92 +91,91 @@ def shell_handler(msg):
     ident         = msg[0]
     delim         = msg[1]
     signature     = msg[2]
-    parent_header = decoder.decode(msg[3])
+    parent_header = decode(msg[3])
     header        = msg[4]
     metadata      = msg[5]
-    content       = decoder.decode(msg[6])
+    content       = decode(msg[6])
 
     # process request:
-    print("Executing:", content["code"])
+    dprint(1, "Executing:", content["code"])
 
     # return reponse:
     content = {
+        "status": "ok",
         "execution_count": execution_count,
-        "data": {"text/plain": "42"},
+        "playload": [],
+        "user_variables": {},
+        "user_expressions": {},
     }
     header_pub = {
-        "msg_id": parent_header["msg_id"],
+        "msg_id": msg_id(),
+        "username": parent_header["username"],
         "session": parent_header["session"],
         "msg_type": "pyout",
     }
     header_reply = {
-        "msg_id": parent_header["msg_id"],
+        "msg_id": msg_id(),
+        "username": parent_header["username"],
         "session": parent_header["session"],
-        "status": "ok",
-        "payload": [{}],
-        "user_variables": {},
-        "user_expressions": {},
         "msg_type": "execute_reply",
     } 
 
     ### respond:
-    msg_lst = [bytes(encoder.encode(header_pub)), # header_pub
+    msg_lst = [bytes(encode(header_pub)), # header_pub
                msg[3],
                msg[5],
-               bytes(encoder.encode(content))]
+               bytes(encode(content))]
     signature = sign(msg_lst)
-    print("msg_list:", msg_lst)
-    print("signature:", signature)
+    dprint(3, "msg_list:", msg_lst) # list of serialized objects
+    dprint(3, "signature:", signature)
     # Send to pub:
     iopub_stream.send_multipart([
-        msg[0], # Ident
         "<IDS|MSG>", # delim
         signature, # HMAC sig
-        bytes(encoder.encode(header_pub)), # header_pub
+        bytes(encode(header_pub)), # header_pub
         msg[3], # parent
         msg[5], # parent_header
-        bytes(encoder.encode(content))])
+        bytes(encode(content))])
     # Send to shell:
-    msg_lst = [bytes(encoder.encode(header_reply)), # header_pub
+    msg_lst = [bytes(encode(header_reply)), # header_pub
                msg[3],
                msg[5],
-               bytes(encoder.encode(content))]
+               bytes(encode(content))]
     signature = sign(msg_lst)
-    print("msg_list:", msg_lst)
-    print("signature:", signature)
+    dprint(3, "msg_list:", msg_lst) # list of serialized objects
+    dprint(3, "signature:", signature)
     shell_stream.send_multipart([
-        msg[0], # Ident
         "<IDS|MSG>", # delim
         signature, # sig
-        bytes(encoder.encode(header_reply)),
+        bytes(encode(header_reply)),
         msg[3],
         msg[5],
-        bytes(encoder.encode(content))])
+        bytes(encode(content))])
     execution_count += 1
 
 def control_handler(msg):
-    print("control received:", msg)
+    dprint(1, "control received:", msg)
 
 def stdin_handler(msg):
-    print("stdin received:", msg)
+    dprint(1, "stdin received:", msg)
 
 exiting = False
 def heartbeat_loop():
     while not exiting:
-        print("Ping!")
+        dprint(2, "Ping!")
         heartbeat_socket.send(b'ping')
         ready = poll()
         if ready:
             heartbeat_socket.recv()
         else:
-            print("heartbeat_loop fail!")
+            dprint(1, "heartbeat_loop fail!")
 
 def poll():
     events = []
-    print("Start poll...")
+    dprint(2, "Start poll...")
     while not exiting:
         try:
-            print(".", end="")
+            dprint(2, ".", end="")
             sys.stdout.flush()
             events = poller.poll(1000)
         except ZMQError as e:
@@ -154,11 +184,11 @@ def poll():
             else:
                 raise
         except Exception:
-            print("Exception!")
+            dprint(2, "Exception!")
             raise
         #else:
         #    break
-    print("Return:", events)
+    dprint(2, "Return:", events)
     return events
 
 ioloop.install()
@@ -176,94 +206,94 @@ ioloop.install()
 
 def shell_thread():
     name = "Shell"
-    print("Starting loop for '%s'..." % name)
+    dprint(2, "Starting loop for '%s'..." % name)
     while True:
-        print("%s Loop!" % name)
+        dprint(2, "%s Loop!" % name)
         try:
             shell_loop.start()
         except ZMQError as e:
-            print("%s ZMQError!" % name)
+            dprint(2, "%s ZMQError!" % name)
             if e.errno == errno.EINTR:
                 continue
             else:
                 raise
         except Exception:
-            print("%s Exception!" % name)
+            dprint(2, "%s Exception!" % name)
             if exiting:
                 break
             else:
                 raise
         else:
-            print("%s Break!" % name)
+            dprint(2, "%s Break!" % name)
             break
 
 def control_thread():
     name = "Control"
-    print("Starting loop for '%s'..." % name)
+    dprint(2, "Starting loop for '%s'..." % name)
     while True:
-        print("%s Loop!" % name)
+        dprint(2, "%s Loop!" % name)
         try:
             control_loop.start()
         except ZMQError as e:
-            print("%s ZMQError!" % name)
+            dprint(2, "%s ZMQError!" % name)
             if e.errno == errno.EINTR:
                 continue
             else:
                 raise
         except Exception:
-            print("%s Exception!" % name)
+            dprint(2, "%s Exception!" % name)
             if exiting:
                 break
             else:
                 raise
         else:
-            print("%s Break!" % name)
+            dprint(2, "%s Break!" % name)
             break
 
 def iopub_thread():
     name = "IOPub"
-    print("Starting loop for '%s'..." % name)
+    dprint(2, "Starting loop for '%s'..." % name)
     while True:
-        print("%s Loop!" % name)
+        dprint(2, "%s Loop!" % name)
         try:
             iopub_loop.start()
         except ZMQError as e:
-            print("%s ZMQError!" % name)
+            dprint(2, "%s ZMQError!" % name)
             if e.errno == errno.EINTR:
                 continue
             else:
                 raise
         except Exception:
-            print("%s Exception!" % name)
+            dprint(2, "%s Exception!" % name)
             if exiting:
                 break
             else:
                 raise
         else:
-            print("%s Break!" % name)
+            dprint(2, "%s Break!" % name)
             break
 
 def stdin_thread():
     name = "StdIn"
-    print("Starting loop for '%s'..." % name)
+    dprint(2, "Starting loop for '%s'..." % name)
     while True:
-        print("%s Loop!" % name)
+        dprint(2, "%s Loop!" % name)
         try:
             stdin_loop.start()
         except ZMQError as e:
-            print("%s ZMQError!" % name)
+            dprint(2, "%s ZMQError!" % name)
             if e.errno == errno.EINTR:
                 continue
             else:
                 raise
         except Exception:
-            print("%s Exception!" % name)
+            dprint(2, "%s Exception!" % name)
             if exiting:
                 break
             else:
                 raise
         else:
-            print("%s Break!" % name)
+            dprint(2, "%s Break!" % name)
             break
 
 ##########################################
@@ -280,7 +310,7 @@ poller.register(heartbeat_socket, zmq.POLLIN)
 # aslo called SubSocketChannel in IPython sources
 iopub_socket = ctx.socket(zmq.SUB)
 iopub_socket.setsockopt(zmq.SUBSCRIBE,b'')
-#iopub_socket.setsockopt(zmq.IDENTITY, session_id)
+iopub_socket.setsockopt(zmq.IDENTITY, session_id)
 iopub_socket.bind(iopub_conn)
 iopub_loop = ioloop.IOLoop()
 iopub_stream = zmqstream.ZMQStream(iopub_socket, iopub_loop)
@@ -289,7 +319,7 @@ iopub_stream.on_recv(iopub_handler)
 ##########################################
 # Control:
 control_socket = ctx.socket(zmq.ROUTER)
-#control_socket.setsockopt(zmq.IDENTITY, session_id)
+control_socket.setsockopt(zmq.IDENTITY, session_id)
 control_socket.bind(control_conn)
 control_loop = ioloop.IOLoop()
 control_stream = zmqstream.ZMQStream(control_socket, control_loop)
@@ -298,7 +328,7 @@ control_stream.on_recv(control_handler)
 ##########################################
 # Stdin:
 stdin_socket = ctx.socket(zmq.ROUTER)
-#stdin_socket.setsockopt(zmq.IDENTITY, session_id)
+stdin_socket.setsockopt(zmq.IDENTITY, session_id)
 stdin_socket.bind(stdin_conn)
 stdin_loop = ioloop.IOLoop()
 stdin_stream = zmqstream.ZMQStream(stdin_socket, stdin_loop)
@@ -307,13 +337,13 @@ stdin_stream.on_recv(stdin_handler)
 ##########################################
 # Shell:
 shell_socket = ctx.socket(zmq.ROUTER)
-#shell_socket.setsockopt(zmq.IDENTITY, session_id)
+shell_socket.setsockopt(zmq.IDENTITY, session_id)
 shell_socket.bind(shell_conn)
 shell_loop = ioloop.IOLoop()
 shell_stream = zmqstream.ZMQStream(shell_socket, shell_loop)
 shell_stream.on_recv(shell_handler)
 
-print("Starting loops...")
+dprint(1, "Starting loops...")
 threads = [threading.Thread(target=shell_thread),
            threading.Thread(target=iopub_thread),
            threading.Thread(target=control_thread),
@@ -321,4 +351,4 @@ threads = [threading.Thread(target=shell_thread),
            threading.Thread(target=heartbeat_loop)]
 for thread in threads:
     thread.start()
-print("Ready! Listening...")
+dprint(1, "Ready! Listening...")
